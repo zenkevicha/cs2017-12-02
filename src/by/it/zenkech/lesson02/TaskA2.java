21d4931ee446456df2679a87e9bf68bf5c44cba8
zenkevicha <LTI1988@mail.ru>
2017-12-07 01:29:34 +0300

    TaskA2 and TaskA3 completed.

--- src/by/it/zenkech/lesson02/TaskA2.java
+++ src/by/it/zenkech/lesson02/TaskA2.java
@@ -8,5 +8,9 @@ package by.it.zenkech.lesson02;
 Для автоматической проверки откройте и запустите класс Testing
 */
 class TaskA2 {
-
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Я начинаю изучать Java!");
+        }
+    }
 }
